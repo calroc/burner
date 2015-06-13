766240c474079f03d35b228ccb389ffc40f297d4
formans <formans@google.com>
2015-06-13 15:03:16 -0700

    Print debuggin FTW.

--- local_cache.py
+++ local_cache.py
@@ -7,6 +7,7 @@ CACHES = {}
 
 def get_cache(path):
   path = abspath(path)
+  print 'get cache of', path
   try:
     return CACHES[path]
   except KeyError:
@@ -19,6 +20,7 @@ class LocalCache(dict):
   def __init__(self, path):
     path = abspath(path)
     self.path = path
+    print 'making cache for', path
     if exists(path):
       with open(path) as data:
         for line in data:
@@ -29,7 +31,9 @@ class LocalCache(dict):
           dict.__setitem__(self, url, tag)
 
   def __setitem__(self, url, tag):
+    print 'setting', url, tag
     dict.__setitem__(self, url, tag)
     if url not in self and len(self) < MAX_SIZE:
+      print 'writing to disk', url, tag
       with open(self.path, 'a') as data:
         print >> data, tag, url
