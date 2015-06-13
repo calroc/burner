59f31094315dfcb046235086cc6992af2b1a7dbc
formans <formans@google.com>
2015-06-13 15:06:24 -0700

    It's always obvious once it's obvious.

--- local_cache.py
+++ local_cache.py
@@ -7,7 +7,6 @@ CACHES = {}
 
 def get_cache(path):
   path = abspath(path)
-  print 'get cache of', path
   try:
     return CACHES[path]
   except KeyError:
@@ -20,7 +19,6 @@ class LocalCache(dict):
   def __init__(self, path):
     path = abspath(path)
     self.path = path
-    print 'making cache for', path
     if exists(path):
       with open(path) as data:
         for line in data:
@@ -31,9 +29,8 @@ class LocalCache(dict):
           dict.__setitem__(self, url, tag)
 
   def __setitem__(self, url, tag):
-    print 'setting', url, tag
-    dict.__setitem__(self, url, tag)
-    if url not in self and len(self) < MAX_SIZE:
-      print 'writing to disk', url, tag
-      with open(self.path, 'a') as data:
-        print >> data, tag, url
+    if url not in self:
+      dict.__setitem__(self, url, tag)
+      if len(self) < MAX_SIZE:
+        with open(self.path, 'a') as data:
+          print >> data, tag, url
