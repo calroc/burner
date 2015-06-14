c9217d3b4bd97025cd58bc7e1cc29ca5cbe6699e
formans <formans@google.com>
2015-06-13 23:09:44 -0700

    Serve get/lookup requests.

--- tryit.py
+++ tryit.py
@@ -1,9 +1,13 @@
 from server import run, Server
-from handlers import make_reg_handler
-from store import get_client, store
+from handlers import make_reg_handler, make_get_handler
+from store import get_client, store, get
 import burnerconf
 
 
 cache = get_client([burnerconf.CACHE_URL])
 handle_registration = make_reg_handler(cache, store)
-run(Server(register=handle_registration))
+handle_lookup = make_get_handler(cache, get)
+run(Server(
+  get=handle_lookup,
+  register=handle_registration,
+  ))
