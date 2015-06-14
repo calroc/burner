9bb5b1d4df4a2ab56c8c9d503fcf6ea27744725f
formans <formans@google.com>
2015-06-13 17:00:22 -0700

    Check urls.

--- tryit.py
+++ tryit.py
@@ -1,13 +1,15 @@
+from urlparse import urlparse
 from local_cache import get_cache
 from store import get_client, store, get
 
 
 LOCAL_CACHE = get_cache('local_cache.txt')
-normalize_url = lambda url: url
 
 
 def register(url, cache, store, local=LOCAL_CACHE):
   url = normalize_url(url)
+  if not url:
+    return
   tag = local.get(url)
   if tag is None:
     print 'local cache miss', url
@@ -16,16 +18,23 @@ def register(url, cache, store, local=LOCAL_CACHE):
   return tag
 
 
-tag = 'ocs2levt5ke6futc2ng9bjn4ohq5sq3'
-url = 'http://calroc.webfactional.com/00000000/00000000'
+def normalize_url(url):
+  try:
+    result = urlparse(url.lower())
+  except:
+    return
+  if (
+    result.scheme in ('http', 'https')
+    and not (result.params or result.query or result.fragment)
+    ):
+    return result.geturl()
 
 
 if __name__ == '__main__':
-  import burnerconf
+  import pprint, burnerconf
+  url = 'http://calroc.webfactional.com/00000000/00000000'
   cache = get_client([burnerconf.CACHE_URL])
   print register('http://dendritenetwork.com/', cache, store)
   print register(url, cache, store)
   print register('http://dendritenetwork.com/', cache, store)
-  print LOCAL_CACHE
-
-
+  pprint(LOCAL_CACHE)
